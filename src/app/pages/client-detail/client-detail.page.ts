19fbb611280d317ab33f4e56b7d886e4f9281834
Jorge Vergara <javebratt@gmail.com>
2018-09-16 17:08:12 -0500

    Adds the client pages

--- src/app/pages/client-detail/client-detail.page.ts
+++ src/app/pages/client-detail/client-detail.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ClientService } from '../../services/client.service';
 
 @Component({
   selector: 'app-client-detail',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-detail.page.scss'],
 })
 export class ClientDetailPage implements OnInit {
+  public client: Observable<any>;
+  public clientWeightTrack: Observable<any>;
 
-  constructor() { }
+  constructor(
+    public router: Router,
+    public route: ActivatedRoute,
+    public clientService: ClientService
+  ) {}
 
   ngOnInit() {
+    this.client = this.clientService
+      .clientDetailShow(this.route.snapshot.paramMap.get('id'))
+      .valueChanges();
+    this.clientWeightTrack = this.clientService
+      .clientWeightHistoryCoach(this.route.snapshot.paramMap.get('id'))
+      .valueChanges();
   }
-
 }
