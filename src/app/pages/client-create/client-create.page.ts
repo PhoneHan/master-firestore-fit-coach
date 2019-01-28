96c6c0335e1e5a07b3e1f874e6b513d8633bd2d0
Jorge Vergara <javebratt@gmail.com>
2019-01-27 20:33:04 -0500

    Adds functions

--- src/app/pages/client-create/client-create.page.ts
+++ src/app/pages/client-create/client-create.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ClientService } from '../../services/client.service';
 
 @Component({
   selector: 'app-client-create',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-create.page.scss'],
 })
 export class ClientCreatePage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  public clientCreateForm: FormGroup;
+  constructor(
+    public router: Router,
+    public clientService: ClientService,
+    public formBuilder: FormBuilder
+  ) {
+    this.clientCreateForm = formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      startingWeight: ['', Validators.required],
+    });
   }
 
+  ngOnInit() {}
+
+  clientCreate(clientCreateForm): void {
+    if (!clientCreateForm.valid) {
+      console.log(clientCreateForm.value);
+    } else {
+      this.clientService
+        .clientCreate(
+          clientCreateForm.value.name,
+          clientCreateForm.value.email,
+          clientCreateForm.value.startingWeight
+        )
+        .then(
+          () => {
+            this.router.navigateByUrl('/home');
+          },
+          error => {
+            console.error(error);
+          }
+        );
+    }
+  }
 }
--- src/app/pages/client-list/client-list.page.ts
+++ src/app/pages/client-list/client-list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../services/client.service';
 
 @Component({
   selector: 'app-client-list',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-list.page.scss'],
 })
 export class ClientListPage implements OnInit {
-
-  constructor() { }
+  public clientList: {};
+  constructor(public clientService: ClientService) {}
 
   ngOnInit() {
+    this.clientService
+      .clientListShow()
+      .valueChanges()
+      .subscribe(client => {
+        this.clientList = client;
+      });
   }
-
 }
